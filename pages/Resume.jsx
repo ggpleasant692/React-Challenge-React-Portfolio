2087c5df5d167e7a249039f0f42d7ba150ddab79
Gee <g.pleasant@lafilm.edu>
2024-10-25 01:10:55 -0400

    updated code with profenciences and resume download

--- pages/Resume.jsx
+++ pages/Resume.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Resume = () => {
-    return (
-        <section>
-            <h2>Resume</h2>
-            <a href="link-to-your-resume.pdf" download>Download Resume</a>
-            <ul>
-                <li>Proficiency 1</li>
-                <li>Proficiency 2</li>
-                {/* Add more proficiencies later */}
-            </ul>
-        </section>
-    );
+  return (
+    <section>
+      <h2>Resume</h2>
+      <a href="link-to-your-resume.pdf" download>
+        Download Resume
+      </a>
+      <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>React.js, Vite</li>
+        <li>Node.js, Express.js, MongoDB</li>
+        <li>Git, GitHub, Gitlab</li>
+        <li>Bootstrap, Tailwind CSS</li>
+        <li>RESTful APIs</li>
+        <li>Responsive Web Design</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Resume;
