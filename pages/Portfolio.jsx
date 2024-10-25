21aec2e0fa88d0aa00a34531bc70f05c66deff2b
Gee <g.pleasant@lafilm.edu>
2024-10-25 00:43:19 -0400

    updated images for portfolio page

--- pages/Portfolio.jsx
+++ pages/Portfolio.jsx
@@ -3,37 +3,37 @@ import React from 'react';
 const projects = [
   {
     title: 'Web API Blog',
-    image: 'my-portfolio/src/assets/blogapi.png', 
+    image: './src/assets/blogapi.png', 
     deployedLink: 'https://ggpleasant692.github.io/task_board_3rdparty_api/',
     githubLink: 'https://github.com/ggpleasant692/task_board_3rdparty_api',
   },
   {
     title: 'SEO Refactoring',
-    image: 'path/to/image2.jpg',
+    image: './src/assets/SEOss.png',
     deployedLink: 'http://127.0.0.1:5500/index.html',
     githubLink: 'https://github.com/ggpleasant692/html-seo-refactor',
   },
   {
     title: 'CSS Portfolio',
-    image: 'path/to/image2.jpg',
+    image: './src/assets/PortfolioSS.png',
     deployedLink: 'https://ggpleasant692.github.io/html-css-portfolio/',
     githubLink: 'https://github.com/ggpleasant692/html-css-portfolio',
   },
   {
     title: 'Javascript Functions',
-    image: 'path/to/image2.jpg',
+    image: './src/assets/JSfunctions.png',
     deployedLink: 'https://ggpleasant692.github.io/javascript-functions/',
     githubLink: 'https://github.com/ggpleasant692/javascript-functions',
   },
   {
     title: 'Event Finder',
-    image: 'path/to/image2.jpg',
+    image: './src/assets/eventfinderss.png',
     deployedLink: 'https://mern-event-finder.onrender.com/',
     githubLink: 'https://github.com/zoniaramirez/MERN-event-finder',
   },
   {
     title: 'Music Trivia',
-    image: 'path/to/image2.jpg',
+    image: './src/assets/musictrivia.jpeg',
     deployedLink: 'https://music-trivia-app.onrender.com/',
     githubLink: 'https://github.com/lllewell/music-trivia-app',
   },
