4dec1246447dad5bb5733cdced0b55d7a2027cdb
Gee <g.pleasant@lafilm.edu>
2024-10-24 23:56:20 -0400

    added code

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutMe from '../pages/AboutMe';
+import Portfolio from '../pages/Portfolio';
+import Contact from '../pages/Contact';
+import Resume from '../pages/Resume';
+
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<AboutMe />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/resume" element={<Resume />} />
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
+    );
+};
+
+export default App;
